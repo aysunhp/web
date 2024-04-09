d3c39c81fa7bc70d580f68960b1f91f077d28af8
aysunhp <bd7zatzha@code.edu.az>
2024-04-09 06:30:35 +0400

    added

---
+++ MVC-WebApplication1/WebApplication1/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+function addToDisplay(value) {
+    document.getElementById('display').value += value;
+}
+
+function calculate() {
+    try {
+        document.getElementById('display').value = eval(document.getElementById('display').value);
+    } catch (error) {
+        document.getElementById('display').value = 'Error';
+    }
+}
+
+function reset() {
+    document.getElementById('display').value = '';
+}
--- webtex-calculator/app.js
+++ webtex-calculator/app.js
@@ -1,15 +1,17 @@
 function addToDisplay(value) {
-    document.getElementById('display').value += value;
+  document.getElementById("display").value += value;
 }
 
 function calculate() {
-    try {
-        document.getElementById('display').value = eval(document.getElementById('display').value);
-    } catch (error) {
-        document.getElementById('display').value = 'Error';
-    }
+  try {
+    document.getElementById("display").value = eval(
+      document.getElementById("display").value
+    );
+  } catch (error) {
+    document.getElementById("display").value = "Error";
+  }
 }
 
 function reset() {
-    document.getElementById('display').value = '';
+  document.getElementById("display").value = "";
 }
